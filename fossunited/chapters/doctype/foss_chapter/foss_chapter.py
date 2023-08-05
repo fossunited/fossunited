9dfa0bc332613304f74bc6e2570acf3ae1430474
Harsh Tandiya <harsh.tandiya@gmail.com>
2023-08-05 22:41:55 +0530

    chore: enabled web views for chapters

--- fossunited/chapters/doctype/foss_chapter/foss_chapter.py
+++ fossunited/chapters/doctype/foss_chapter/foss_chapter.py
@@ -1,8 +1,10 @@
 # Copyright (c) 2023, Frappe x FOSSUnited and contributors
 # For license information, please see license.txt
 
-# import frappe
 from frappe.model.document import Document
+
+# import frappe
+from frappe.website.website_generator import WebsiteGenerator
 
 
 class FOSSChapter(Document):
