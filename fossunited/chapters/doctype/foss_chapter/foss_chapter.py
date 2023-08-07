97d73574a53f143377f2da7e9be7b765693ffe20
Harsh Tandiya <harsh.tandiya@gmail.com>
2023-08-07 09:50:20 +0530

    Revert "chore: enabled web views for chapters"
    This reverts commit 9dfa0bc332613304f74bc6e2570acf3ae1430474.

--- fossunited/chapters/doctype/foss_chapter/foss_chapter.py
+++ fossunited/chapters/doctype/foss_chapter/foss_chapter.py
@@ -1,10 +1,8 @@
 # Copyright (c) 2023, Frappe x FOSSUnited and contributors
 # For license information, please see license.txt
 
-from frappe.model.document import Document
-
 # import frappe
-from frappe.website.website_generator import WebsiteGenerator
+from frappe.model.document import Document
 
 
 class FOSSChapter(Document):
