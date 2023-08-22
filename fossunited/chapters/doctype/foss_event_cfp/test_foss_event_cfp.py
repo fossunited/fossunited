9c5188890df32c01336a6b6f66d46bfa134475a9
Harsh Tandiya <harsh.tandiya@gmail.com>
2023-08-22 15:01:21 +0530

    feat: event cfp, sponsor, schedule doctypes added

--- fossunited/chapters/doctype/foss_event_cfp/test_foss_event_cfp.py
+++ fossunited/chapters/doctype/foss_event_cfp/test_foss_event_cfp.py
@@ -5,5 +5,5 @@
 from frappe.tests.utils import FrappeTestCase
 
 
-class TestFOSSChapterTalkProposal(FrappeTestCase):
+class TestFOSSEventCFP(FrappeTestCase):
 	pass
--- fossunited/fossunited/doctype/foss_event_sponsors/foss_event_sponsors.py
+++ fossunited/fossunited/doctype/foss_event_sponsors/foss_event_sponsors.py
@@ -5,5 +5,5 @@
 from frappe.model.document import Document
 
 
-class FOSSChapterTalkProposal(Document):
+class FOSSEventSponsors(Document):
 	pass
