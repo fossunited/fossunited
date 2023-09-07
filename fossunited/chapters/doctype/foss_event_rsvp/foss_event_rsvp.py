0666993e981ba89a1c84519df0918457f4b33ee8
Harsh Tandiya <harsh.tandiya@gmail.com>
2023-09-08 01:25:25 +0530

    feat: event rsvp doctype added

---
+++ fossunited/chapters/doctype/foss_event_rsvp/foss_event_rsvp.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Frappe x FOSSUnited and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.website.website_generator import WebsiteGenerator
+
+
+class FOSSEventRSVP(WebsiteGenerator):
+	pass
