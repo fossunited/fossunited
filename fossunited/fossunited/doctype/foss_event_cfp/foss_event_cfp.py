35151332efd0500e39d981ee30b97165429e42d8
Harsh Tandiya <harsh.tandiya@gmail.com>
2023-09-21 02:05:00 +0530

    feat: cfp web form live

--- fossunited/fossunited/doctype/foss_event_cfp/foss_event_cfp.py
+++ fossunited/fossunited/doctype/foss_event_cfp/foss_event_cfp.py
@@ -2,8 +2,12 @@
 # For license information, please see license.txt
 
 # import frappe
-from frappe.model.document import Document
+from frappe.website.website_generator import WebsiteGenerator
 
 
-class FOSSEventCFP(Document):
-	pass
+class FOSSEventCFP(WebsiteGenerator):
+	def before_save(self):
+		self.self_route()
+
+	def self_route(self):
+		self.route = f"events/{self.event}/cfp"
