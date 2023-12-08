8303c5cbbc26f47443feaa70102954cd858ffc76
mangeshrex <mangeshrex@tutanota.com>
2023-12-09 03:03:26 +0530

    add: newsletter web page with subscribe feature

---
+++ fossunited/www/newsletter/index.js
@@ -0,0 +1,43 @@
+function subscribeHandler(event) {
+  var emailInput = document.getElementById("email").value;
+
+  frappe.call({
+    method: "frappe.client.insert",
+    args: {
+      doc: {
+        doctype: "Email Group Member",
+        email: emailInput,
+        email_group: "FOSSU Newsletter",
+      },
+    },
+    callback: (r) => {
+      console.log(r);
+    },
+    error: (e) => {
+      console.error(e);
+    },
+  });
+  event.stopPropagation();
+  document
+    .getElementById("subscribe")
+    .removeEventListener("click", subscribeHandler);
+}
+
+document
+  .getElementById("subscribe")
+  .addEventListener("click", subscribeHandler);
+
+function showSuccessMsg() {
+  setTimeout( function (){
+    document.getElementById("subscribed-page").style.display = 'block';
+  }, 500);
+
+  var sections = ["header", "newsletters-list", "footer-cta", "subscribe-section"];
+  for (var i=0; i < sections.length; i++) {
+    document.getElementById(sections[i]).style.display = 'none';
+  }
+}
+
+document.getElementById('subscribe').addEventListener('click', function () {
+  showSuccessMsg();
+});
