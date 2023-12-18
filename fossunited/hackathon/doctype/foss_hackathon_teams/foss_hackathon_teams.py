3a1b97b76c02938112bbab25536dabd35dab6a63
RiyaMathew-11 <riyapmathew2000@gmail.com>
2023-12-18 18:35:18 +0530

    chore: name team creator as team lead

--- fossunited/hackathon/doctype/foss_hackathon_teams/foss_hackathon_teams.py
+++ fossunited/hackathon/doctype/foss_hackathon_teams/foss_hackathon_teams.py
@@ -1,9 +1,24 @@
 # Copyright (c) 2023, Frappe x FOSSUnited and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.website.website_generator import WebsiteGenerator
 
 
 class FOSSHackathonTeams(WebsiteGenerator):
-    pass
+    def after_insert(self):
+        participant_hash = frappe.db.get_value(
+            "FOSS Hackathon Participant",
+            {
+                "participant_email": frappe.session.user,
+                "hackathon_name": self.hackathon_name,
+            },
+            "name",
+        )
+        print(participant_hash)
+        team_owner_doc = frappe.get_doc(
+            "FOSS Hackathon Participant", participant_hash
+        )
+        team_owner_doc.team_name = self.name
+        team_owner_doc.is_lead = 1
+        team_owner_doc.save()
