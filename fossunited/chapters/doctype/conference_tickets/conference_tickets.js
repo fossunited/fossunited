d56062b3c995b40787f3f56f04d33f57a2e74fc2
mangeshrex <mangeshrex@tutanota.com>
2024-01-27 23:57:59 +0530

    add: Conference TIckets doctype and update events

---
+++ fossunited/chapters/doctype/conference_tickets/conference_tickets.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Frappe x FOSSUnited and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Conference Tickets", {
+// 	refresh(frm) {
+
+// 	},
+// });
