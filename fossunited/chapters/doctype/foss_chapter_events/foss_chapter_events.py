5253d3818f1eeacfa4d6b5b42963afb696210e87
mangeshrex <mangeshrex@tutanota.com>
2024-01-29 18:09:37 +0530

    fix: doctype permissions

--- fossunited/chapters/doctype/foss_chapter_events/foss_chapter_events.py
+++ fossunited/chapters/doctype/foss_chapter_events/foss_chapter_events.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2023, Frappe x FOSSUnited and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.website.website_generator import WebsiteGenerator
 
 
