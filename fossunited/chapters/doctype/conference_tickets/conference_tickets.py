d8a3b9ac91ec52996eb207d94000832e98679f64
mangeshrex <mangeshrex@tutanota.com>
2024-02-02 01:43:21 +0530

    chore: automating email group member workflow

--- fossunited/chapters/doctype/conference_tickets/conference_tickets.py
+++ fossunited/chapters/doctype/conference_tickets/conference_tickets.py
@@ -9,4 +9,17 @@ from frappe.model.document import Document
 
 
 class ConferenceTickets(Document):
-    pass
+    # function to validate the payment.
+    def on_update(self):
+        if self.payment_captured == True:
+            self.add_user_to_list()
+
+    def add_user_to_list(self):
+        print(self.event_name)
+        details = frappe.get_doc('Conference Tickets', self.name )
+
+        add_user = frappe.get_doc({
+            'doctype': 'Email Group Member',
+            'email_group': details.event_name, 
+            'email': details.email 
+        }).insert(ignore_permissions=True)
