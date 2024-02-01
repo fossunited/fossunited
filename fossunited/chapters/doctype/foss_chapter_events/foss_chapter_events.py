633ba39bf52650f7bafaa93ee22dde1abbc117d7
mangeshrex <mangeshrex@tutanota.com>
2024-02-02 01:32:45 +0530

    chore: automate email group creation

--- fossunited/chapters/doctype/foss_chapter_events/foss_chapter_events.py
+++ fossunited/chapters/doctype/foss_chapter_events/foss_chapter_events.py
@@ -6,4 +6,17 @@ from frappe.website.website_generator import WebsiteGenerator
 
 
 class FOSSChapterEvents(WebsiteGenerator):
-    pass
+    def on_update(self):
+        if self.status == "Approved":
+            self.create_email_group()
+
+        if self.status == "Cancelled":
+            self.delete_email_group()
+
+    def create_email_group(self):
+        new_list = frappe.new_doc("Email Group")
+        new_list.title = self.event_name
+        new_list.insert()
+
+    def delete_email_group(self):
+        del_list = frappe.delete_doc("Email Group", self.event_name)
