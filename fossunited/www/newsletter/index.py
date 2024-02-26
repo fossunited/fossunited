c761577d870680372e1dba664768590100d27973
mangeshrex <mangeshrex@tutanota.com>
2024-02-27 02:10:36 +0530

    feat: converting decimal datetime into standardised way

--- fossunited/www/newsletter/index.py
+++ fossunited/www/newsletter/index.py
@@ -1,10 +1,19 @@
 import frappe
+import datetime
 
 
 def get_context(context):
-    context.newsletters = frappe.get_all(
+    newsletters = frappe.get_all(
         "Newsletter",
         filters={"published": 1},
-        fields=["name", "subject", "route"],
+        fields=["name", "subject", "route", "email_sent_at"],
         order_by="creation desc",
     )
+
+    for newsletter in newsletters:
+        if newsletter["email_sent_at"]:
+            newsletter["email_sent_at"] = newsletter["email_sent_at"].strftime("%d %B, %Y")            
+            print(newsletter["email_sent_at"])
+            
+    context.newsletters = newsletters
+
