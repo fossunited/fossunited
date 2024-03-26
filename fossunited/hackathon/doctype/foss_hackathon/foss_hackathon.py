cf2e308d67feb72020b0d00745e188f33f078989
harshtandiya <harsh.tandiya@gmail.com>
2024-03-26 15:45:31 +0530

    chore!: remove `hackathon` module
    `hackathon` module was replaced by `foss_hackathon` module

--- fossunited/hackathon/doctype/foss_hackathon/foss_hackathon.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2023, Frappe x FOSSUnited and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.website.website_generator import WebsiteGenerator
-
-
-class FOSSHackathon(WebsiteGenerator):
-    pass
