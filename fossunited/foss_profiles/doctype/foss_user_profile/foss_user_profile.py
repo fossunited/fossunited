358966283cf4ba031c9d3d74756c625cc4b8c872
Md Hussain Nagaria <34810212+NagariaHussain@users.noreply.github.com>
2024-04-07 12:40:30 +0530

    perf: use tuple instead of list

--- fossunited/foss_profiles/doctype/foss_user_profile/foss_user_profile.py
+++ fossunited/foss_profiles/doctype/foss_user_profile/foss_user_profile.py
@@ -17,10 +17,10 @@ class FOSSUserProfile(WebsiteGenerator):
         self.route = self.username.lower().replace("-", "_")
 
     def get_context(self, context):
-        if self.is_private and frappe.session.user not in [
+        if self.is_private and frappe.session.user not in (
             "Administrator",
             self.user,
-        ]:
+        ):
             frappe.throw(
                 _("Profile Not Found"), frappe.DoesNotExistError
             )
