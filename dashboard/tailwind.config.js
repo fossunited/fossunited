8838b53a2a7390b0b16fd1a99c408b240fab4bc9
harshtandiya <harsh.tandiya@gmail.com>
2024-04-17 00:14:17 +0530

    fix(dashboard): extend fff forward font

--- dashboard/tailwind.config.js
+++ dashboard/tailwind.config.js
@@ -5,6 +5,12 @@ module.exports = {
     './src/**/*.{vue,js,ts,jsx,tsx}',
     './node_modules/frappe-ui/src/components/**/*.{vue,js,ts,jsx,tsx}',
   ],
-  theme: {},
+  theme: {
+    extend: {
+      fontFamily: {
+        fff: ['"FFF Forward"', 'sans-serif'],
+      },
+    },
+  },
   plugins: [],
 }
