073fb92365e443f20a943a9eec13573c4e6a80d0
harshtandiya <harsh.tandiya@gmail.com>
2024-04-21 20:16:05 +0530

    chore(dashboard): make chapter card clickable

--- dashboard/src/components/ChapterCard.vue
+++ dashboard/src/components/ChapterCard.vue
@@ -1,25 +1,19 @@
 <template>
-    <Card>
+    <Card
+        class="hover:border-2 hover:border-gray-500 transition-all hover:cursor-pointer"
+        @click="() => $router.push(`/chapter/${props.chapter.name}`)"
+    >
         <template #actions-left>
             <FossClubLogo v-if="props.chapter.chapter_type == 'FOSS Club'" class="w-7 h-7 "></FossClubLogo>
             <CityComunityBranding v-else>{{ props.chapter.chapter_type }}</CityComunityBranding>
         </template>
         <div class="flex justify-between items-baseline">
             <div class="text-lg font-medium">{{ props.chapter.chapter_name }}</div>
-            <Button
-                :variant="'ghost'"
-                :size="'sm'"
-                label="Manage"
-                iconRight="chevron-right"
-                :route="'/chapter/' + props.chapter.name"
-            >
-            </Button>
         </div>
     </Card>
 </template>
 
 <script setup>
-import Button from 'frappe-ui/src/components/Button.vue';
 import FossClubLogo from '@/components/FossClubLogo.vue';
 import CityComunityBranding from '@/components/CityCommunityBranding.vue';
 
