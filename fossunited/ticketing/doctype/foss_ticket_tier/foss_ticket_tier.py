1b157528c87159b2e9ba534e97149fb2a4065fcd
Hussain Nagaria <hussainbhaitech@gmail.com>
2024-04-27 13:09:13 +0530

    chore: ticket tiers

---
+++ fossunited/ticketing/doctype/foss_ticket_tier/foss_ticket_tier.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Frappe x FOSSUnited and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class FOSSTicketTier(Document):
+    pass
