b04c6e80f21919ecc66f13a344b9ecf0da13a3d4
harshtandiya <harsh.tandiya@gmail.com>
2024-04-30 15:03:49 +0530

    fix: hackathon doctype WebsiteGenerator hook

--- fossunited/foss_hackathon/doctype/foss_hackathon/foss_hackathon.py
+++ fossunited/foss_hackathon/doctype/foss_hackathon/foss_hackathon.py
@@ -2,10 +2,10 @@
 # For license information, please see license.txt
 
 # import frappe
-from frappe.model.document import Document
+from frappe.website.website_generator import WebsiteGenerator
 
 
-class FOSSHackathon(Document):
+class FOSSHackathon(WebsiteGenerator):
     def before_save(self):
         self.set_route()
 
