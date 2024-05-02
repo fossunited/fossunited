d9c126d4b9131d1478fed2acab5dd9b0a42bbad9
Hussain Nagaria <hussainbhaitech@gmail.com>
2024-05-02 12:42:52 +0530

    feat: refund from desk

--- fossunited/payments/doctype/razorpay_payment/razorpay_payment.js
+++ fossunited/payments/doctype/razorpay_payment/razorpay_payment.js
@@ -1,8 +1,17 @@
 // Copyright (c) 2024, Frappe x FOSSUnited and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Razorpay Payment", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Razorpay Payment", {
+	refresh(frm) {
+        if (frm.doc.status === "Captured") {
+            frm.add_custom_button("Refund", () => {
+                frm.call("refund").then(({message}) => {
+                    if (message != "failed") {
+                        frappe.show_alert("Refund Processed")
+                        frm.refresh()
+                    }
+                })
+            })
+        }
+	},
+});
