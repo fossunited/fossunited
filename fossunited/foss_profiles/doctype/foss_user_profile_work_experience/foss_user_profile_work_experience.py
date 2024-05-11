364593dada03c244f5dd06e3ec364a6a3070c46d
harshtandiya <harsh.tandiya@gmail.com>
2024-05-12 02:02:44 +0530

    chore(profile): remove URL validation for company website

--- fossunited/foss_profiles/doctype/foss_user_profile_work_experience/foss_user_profile_work_experience.py
+++ fossunited/foss_profiles/doctype/foss_user_profile_work_experience/foss_user_profile_work_experience.py
@@ -6,4 +6,35 @@ from frappe.model.document import Document
 
 
 class FOSSUserProfileWorkExperience(Document):
+    # begin: auto-generated types
+    # This code is auto-generated. Do not modify anything in this block.
+
+    from typing import TYPE_CHECKING
+
+    if TYPE_CHECKING:
+        from frappe.types import DF
+
+        city: DF.Data | None
+        company: DF.Data
+        company_website: DF.Data
+        country: DF.Data | None
+        description: DF.TextEditor | None
+        employment_type: DF.Literal[
+            "",
+            "Full-time",
+            "Part-time",
+            "Internship",
+            "Freelance",
+            "Self-employed",
+            "Trainee",
+        ]
+        end_date: DF.Date | None
+        is_remote: DF.Check
+        is_working_here: DF.Check
+        parent: DF.Data
+        parentfield: DF.Data
+        parenttype: DF.Data
+        start_date: DF.Date
+        title: DF.Data
+    # end: auto-generated types
     pass
