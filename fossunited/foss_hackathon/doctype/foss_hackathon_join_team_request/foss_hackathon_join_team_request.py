39d5a528de8e9a48de0808caf3dca72638d8c4df
harshtandiya <harsh.tandiya@gmail.com>
2024-06-05 17:26:58 +0530

    chore(hackathon): add member to team when request is accepted

--- fossunited/foss_hackathon/doctype/foss_hackathon_join_team_request/foss_hackathon_join_team_request.py
+++ fossunited/foss_hackathon/doctype/foss_hackathon_join_team_request/foss_hackathon_join_team_request.py
@@ -28,14 +28,18 @@ class FOSSHackathonJoinTeamRequest(Document):
 
     def before_save(self):
         if self.has_value_changed("status"):
-            self.add_member_to_team()
+            if self.status == "Accepted":
+                self.add_member_to_team()
 
     def add_member_to_team(self):
         # get participant doc
         try:
             participant_doc = frappe.get_doc(
                 "FOSS Hackathon Participant",
-                {"email": self.email, "hackathon": self.hackathon},
+                {
+                    "email": self.reciever_email,
+                    "hackathon": self.hackathon,
+                },
             )
         except frappe.DoesNotExistError:
             frappe.throw("Participant not found")
