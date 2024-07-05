c4d01011220788be6ee3e9ca5fc9ea82c62a1573
RiyaMathew-11 <riyapmathew2000@gmail.com>
2024-07-05 13:31:59 +0530

    chore: create doctype for restricted usernames

---
+++ fossunited/foss_profiles/doctype/restricted_usernames/restricted_usernames.py
@@ -0,0 +1,20 @@
+# Copyright (c) 2024, Frappe x FOSSUnited and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class RestrictedUsernames(Document):
+    # begin: auto-generated types
+    # This code is auto-generated. Do not modify anything in this block.
+
+    from typing import TYPE_CHECKING
+
+    if TYPE_CHECKING:
+        from frappe.types import DF
+
+        username: DF.Data | None
+    # end: auto-generated types
+
+    pass
