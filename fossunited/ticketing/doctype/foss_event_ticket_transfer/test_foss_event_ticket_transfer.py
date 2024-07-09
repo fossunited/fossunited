86eaa19b946dd406e389fb8ddb83c82da430bc05
harshtandiya <harsh.tandiya@gmail.com>
2024-07-09 14:45:35 +0530

    test(ticket-transfer): add test to check transfer status on creation

--- fossunited/ticketing/doctype/foss_event_ticket_transfer/test_foss_event_ticket_transfer.py
+++ fossunited/ticketing/doctype/foss_event_ticket_transfer/test_foss_event_ticket_transfer.py
@@ -77,3 +77,43 @@ class TestFOSSEventTicketTransfer(FrappeTestCase):
         transfer.delete()
         ticket.delete()
         event.delete()
+
+    def test_check_status_pending_on_create(self):
+        # Given an event and a ticket linked to the event
+        event = frappe.get_doc(
+            {
+                "doctype": "FOSS Chapter Event",
+                "event_name": "Test Event",
+                "event_permalink": "test_perma_2",
+                "status": "Approved",
+                "event_type": "Conference",
+                "event_start_date": datetime.today(),
+                "event_end_date": datetime.today() + timedelta(1),
+                "event_description": "testing",
+            }
+        )
+        event.insert()
+
+        # With a ticket created for a user, try to transfer this ticket to another user while passing "Completed" as the status
+        ticket = frappe.get_doc(
+            {
+                "doctype": "FOSS Event Ticket",
+                "event": event.name,
+                "full_name": "Harsh Tandiya",
+                "email": "harsh2@test.xyz",
+            }
+        )
+        ticket.insert()
+
+        # Then verify that this operation raises a ValidationError
+        with self.assertRaises(frappe.exceptions.ValidationError):
+            transfer = frappe.get_doc(
+                {
+                    "doctype": "FOSS Event Ticket Transfer",
+                    "ticket": ticket.name,
+                    "receiver_name": "Rahul",
+                    "receiver_email": "rahul2@test.xyz",
+                    "status": "Completed",
+                }
+            )
+            transfer.insert()
