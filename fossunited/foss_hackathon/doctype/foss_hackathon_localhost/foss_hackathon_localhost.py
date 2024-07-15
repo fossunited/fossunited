66c785ec20ee1e1201da49fdeceb4da76a470aee
RiyaMathew-11 <riyapmathew2000@gmail.com>
2024-07-15 14:09:47 +0530

    chore: fix nonetype error for fresh localhost creation

--- fossunited/foss_hackathon/doctype/foss_hackathon_localhost/foss_hackathon_localhost.py
+++ fossunited/foss_hackathon/doctype/foss_hackathon_localhost/foss_hackathon_localhost.py
@@ -49,7 +49,9 @@ class FOSSHackathonLocalHost(Document):
 
     def check_if_member_removed(self):
         prev_doc = self.get_doc_before_save()
-        if not len(prev_doc.organizers) > len(self.organizers):
+        if not prev_doc or not len(prev_doc.organizers) > len(
+            self.organizers
+        ):
             return
 
         for member in prev_doc.organizers:
