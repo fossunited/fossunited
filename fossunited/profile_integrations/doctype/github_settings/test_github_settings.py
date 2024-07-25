b6b3f868660f6c227e4a89b5500267c2d6310c67
harshtandiya <harsh.tandiya@gmail.com>
2024-07-25 13:40:57 +0530

    feat(integration): add github settings doctype

---
+++ fossunited/profile_integrations/doctype/github_settings/test_github_settings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Frappe x FOSSUnited and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestGithubSettings(FrappeTestCase):
+    pass
