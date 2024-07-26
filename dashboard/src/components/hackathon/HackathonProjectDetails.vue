51e9ae721dd353f67598b4bc67a082aec6217459
harshtandiya <harsh.tandiya@gmail.com>
2024-07-26 11:51:58 +0530

    chore(dashboard): replace github icon svg with GithubIcon.vue

--- dashboard/src/components/hackathon/HackathonProjectDetails.vue
+++ dashboard/src/components/hackathon/HackathonProjectDetails.vue
@@ -15,23 +15,7 @@
       :disabled="project.data.is_partner_project"
     >
       <template #prefix>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          class="icon w-5 icon-tabler icons-tabler-outline icon-tabler-brand-github"
-        >
-          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-          <path
-            d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"
-          />
-        </svg>
+        <GithubIcon classes="w-4"/>
       </template>
     </FormControl>
     <FormControl label="Demo Link" v-model="project.data.demo_link" />
@@ -61,6 +45,7 @@
 <script setup>
 import CopyToClipboard from '@/components/CopyToClipboardComponent.vue'
 import TextEditor from '@/components/TextEditor.vue'
+import GithubIcon from '@/components/icons/GithubIcon.vue'
 import { defineProps, defineEmits, ref } from 'vue'
 import { FormControl, ErrorMessage, createResource } from 'frappe-ui'
 import { toast } from 'vue-sonner'
