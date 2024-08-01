ffc8e4bab1425dee7fb071f1faf39468837ddc25
harshtandiya <harsh.tandiya@gmail.com>
2024-08-01 12:22:40 +0530

    fix(rsvp-dashboard): weird tab behaviour fixed

--- dashboard/src/pages/EventRsvp.vue
+++ dashboard/src/pages/EventRsvp.vue
@@ -3,23 +3,26 @@
     <EventHeader
       class="px-4 py-8 md:p-8"
       :event="event"
-      :form_exists="rsvp_exists"
+      :form_exists="new Boolean(has_rsvp.data)"
       :form="event_rsvp"
     />
     <TabsWithRoute :tabs="tabs.options" />
-    <RouterView @rsvp-created="rsvpCreated" :event_rsvp="event_rsvp" />
+    <RouterView
+      :event_rsvp="event_rsvp"
+      @rsvp-created="rsvpCreated"
+    />
   </div>
 </template>
-Show
 <script setup>
+import TabsWithRoute from '@/components/TabsWithRoute.vue'
 import EventHeader from '@/components/EventHeader.vue'
 import { createDocumentResource, createResource } from 'frappe-ui'
 import { useRoute, useRouter } from 'vue-router'
-import TabsWithRoute from '@/components/TabsWithRoute.vue'
 import { reactive, ref, watch } from 'vue'
 
 const route = useRoute()
 const router = useRouter()
+
 const event = createDocumentResource({
   doctype: 'FOSS Chapter Event',
   name: route.params.id,
@@ -27,67 +30,70 @@ const event = createDocumentResource({
   auto: true,
 })
 
-let tabs = reactive({
+const tabs = reactive({
   options: [
     {
       label: 'Overview',
       route: `/event/${route.params.id}/rsvp`,
     },
-    {
-      label: 'Web Form',
-      route: `/event/${route.params.id}/rsvp/create`,
-    },
   ],
 })
 
-const replaceCreateOption = () => {
-  // TODO: simplify this
-  tabs.options = tabs.options.filter((d) => d.label !== 'Web Form')
-
-  if (!tabs.options.find((d) => d.label === 'Web Form')) {
-    tabs.options.push({
-      label: 'Web Form',
-      route: `/event/${route.params.id}/rsvp/edit`,
-    })
-  }
-
-  if (!tabs.options.find((d) => d.label === 'Insights')) {
-    tabs.options.push({
-      label: 'Insights',
-      route: `/event/${route.params.id}/rsvp/insights`,
-    })
-  }
-}
-
-let rsvp_exists = ref(false)
-let event_rsvp = reactive({})
-
-watch(event, (newEvent) => {
-  event_rsvp = createResource({
-    url: 'frappe.client.get',
-    params: {
+const has_rsvp = createResource({
+  url: 'frappe.client.get_count',
+  makeParams() {
+    return {
       doctype: 'FOSS Event RSVP',
       filters: {
-        event: newEvent.doc.name,
+        event: route.params.id,
       },
-    },
-    auto: true,
-    onError(error) {
-      if (error.response.status === 404) {
-        rsvp_exists.value = false
-      }
-    },
-    onSuccess(response) {
-      rsvp_exists.value = true
-    },
-  })
-  if (rsvp_exists.value) {
-    replaceCreateOption()
-  }
+    }
+  },
+  auto: true,
+  onSuccess(data) {
+    resetTabs()
+    if (data > 0) {
+      event_rsvp.fetch()
+      tabs.options.push({
+        label: 'Web Form',
+        route: `/event/${route.params.id}/rsvp/edit`,
+      })
+      tabs.options.push({
+        label: 'Insights',
+        route: `/event/${route.params.id}/rsvp/insights`,
+      })
+      return
+    }
+    tabs.options.push({
+      label: 'Web Form',
+      route: `/event/${route.params.id}/rsvp/create`,
+    })
+  },
 })
 
+const event_rsvp = createResource({
+  url: 'frappe.client.get',
+  makeParams() {
+    return {
+      doctype: 'FOSS Event RSVP',
+      filters: {
+        event: route.params.id,
+      },
+    }
+  },
+})
+
+const resetTabs = () => {
+  tabs.options = [
+    {
+      label: 'Overview',
+      route: `/event/${route.params.id}/rsvp`,
+    },
+  ]
+}
+
 const rsvpCreated = () => {
-  replaceCreateOption()
-  router.replace(`/event/${route.params.id}/rsvp/edit`)
+  has_rsvp.fetch()
+  router.replace(`/event/${route.params.id}/rsvp`)
 }
 </script>
