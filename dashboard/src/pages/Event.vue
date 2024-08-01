202296d25aee533cd9e4717b03a036938af81ddb
harshtandiya <harsh.tandiya@gmail.com>
2024-08-01 17:41:10 +0530

    chore(dashboard)!: update sidenavbar component for event manage page

--- dashboard/src/pages/Event.vue
+++ dashboard/src/pages/Event.vue
@@ -1,35 +1,57 @@
 <template>
-  <Header />
   <div class="flex">
-    <AppSidebar title="MANAGE EVENT" :menuItems="sidebarMenuItems" />
-    <RouterView />
+    <SideNavbar
+      title="Manage Event"
+      :menuItems="sidebarMenuItems"
+      :class="showNav ? 'z-50 block mt-[55px]' : 'hidden md:block'"
+    />
+    <div class="w-full md:ml-[220px]">
+      <HeaderWithNav @toggleSidebar="($event) => (showNav = $event)" />
+      <RouterView />
+    </div>
   </div>
 </template>
 
 <script setup>
-import { RouterView, useRoute } from 'vue-router'
-import Header from '@/components/Header.vue'
-import AppSidebar from '@/components/AppSidebar.vue'
+import { ref } from 'vue'
 import { usePageMeta } from 'frappe-ui'
+import { RouterView, useRoute } from 'vue-router'
+import SideNavbar from '@/components/NewAppSidebar.vue'
+import HeaderWithNav from '@/components/HeaderWithNav.vue'
 
 const route = useRoute()
 
+const showNav = ref(false)
+
 const sidebarMenuItems = [
   {
-    label: 'Details',
-    route: `/event/${route.params.id}`,
+    items: [
+      {
+        icon: 'arrow-left',
+        label: 'Go to Dashboard',
+        route: '/chapter'
+      }
+    ]
   },
   {
-    label: 'RSVP',
-    route: `/event/${route.params.id}/rsvp`,
-  },
-  {
-    label: 'CFP',
-    route: `/event/${route.params.id}/cfp`,
-  },
-  {
-    label: 'Volunteers',
-    route: `/event/${route.params.id}/volunteers`,
+    items: [
+      {
+        label: 'Details',
+        route: `/event/${route.params.id}`,
+      },
+      {
+        label: 'RSVP',
+        route: `/event/${route.params.id}/rsvp`,
+      },
+      {
+        label: 'CFP',
+        route: `/event/${route.params.id}/cfp`,
+      },
+      {
+        label: 'Volunteers',
+        route: `/event/${route.params.id}/volunteers`,
+      },
+    ],
   },
 ]
 
