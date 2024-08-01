07daa07f1d60ad3ed713b9ecd3dde6360c258b67
harshtandiya <harsh.tandiya@gmail.com>
2024-08-01 17:39:44 +0530

    chore(dashboard)!: update SideNavbar component to use menuItems prop instead of nav_items.data

--- dashboard/src/components/NewAppSidebar.vue
+++ dashboard/src/components/NewAppSidebar.vue
@@ -1,36 +1,37 @@
 <template>
   <div
-    v-if="nav_items.data"
     class="relative block min-h-0 flex-shrink-0 overflow-hidden hover:overflow-auto"
   >
     <div class="fixed flex min-h-screen w-[220px] flex-col border-r bg-gray-50">
       <div class="p-4">
-        <!-- <FOSSUnitedLogo class="w-16 h-12" fill="black"></FOSSUnitedLogo> -->
         <div class="font-fff text-gray-900 uppercase">FOSS United</div>
         <div class="text-sm mt-2 tracking-wider text-gray-700 uppercase">
           Dashboard
         </div>
       </div>
-      <div class="flex flex-col gap-2 px-4 my-2" v-if="nav_items.data">
-        <div v-for="group in nav_items.data" class="my-1">
+      <div class="text-lg font-semibold uppercase px-4 mt-4" v-if="title">{{ title }}</div>
+      <div class="flex flex-col gap-2 px-4 my-2" v-if="menuItems.length > 0">
+        <div v-for="group in menuItems" class="my-1">
           <div
             class="text-xs text-gray-600 font-medium uppercase tracking-wide"
+            v-if="group.parent_label"
           >
             {{ group.parent_label }}
           </div>
-          <div class="flex flex-col my-1 text-gray-600">
+          <div class="flex flex-col my-1 gap-2 text-gray-700">
             <router-link
               v-for="(item, index) in group.items"
               :key="item.label"
               :to="item.route"
-              class="w-full text-sm rounded-sm p-2 hover:bg-gray-100"
+              class="w-full text-sm flex items-center gap-1 rounded-sm p-2 hover:bg-gray-100 transition-colors "
               :class="
                 isMenuItemActive(item.route, index)
                   ? 'font-medium text-gray-900 bg-gray-100'
                   : ''
               "
             >
-              {{ item.label }}
+              <FeatherIcon class="w-4 h-4" v-if="item.icon" :name="item.icon"/>
+              <span>{{ item.label }}</span>
             </router-link>
           </div>
         </div>
@@ -39,15 +40,21 @@
   </div>
 </template>
 <script setup>
-import FOSSUnitedLogo from '@/components/FossUnitedLogo.vue'
-import { createResource } from 'frappe-ui'
+import { createResource, FeatherIcon } from 'frappe-ui'
 import { useRoute } from 'vue-router'
+import { defineProps } from 'vue'
 
 const route = useRoute()
 
-const nav_items = createResource({
-  url: 'fossunited.api.sidebar.get_sidebar_items',
-  auto: true,
+const props = defineProps({
+  title: {
+    type: String,
+  },
+  menuItems: {
+    type: Array,
+    required: true,
+    default: () => [],
+  },
 })
 
 const isMenuItemActive = (menuRoute, index) => {
