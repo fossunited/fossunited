29f1ff506d48550816f1e8c16b273b0dd0e363e7
RiyaMathew-11 <riyapmathew2000@gmail.com>
2024-08-08 15:02:46 +0530

    chore: add additional fields and options in IP doctype

--- fossunited/fossunited/doctype/industry_partners/industry_partners.py
+++ fossunited/fossunited/doctype/industry_partners/industry_partners.py
@@ -5,4 +5,37 @@ from frappe.model.document import Document
 
 
 class IndustryPartners(Document):
+    # begin: auto-generated types
+    # This code is auto-generated. Do not modify anything in this block.
+
+    from typing import TYPE_CHECKING
+
+    if TYPE_CHECKING:
+        from frappe.types import DF
+
+        company: DF.Data
+        fosshack_sponsor_tier: DF.Literal[
+            "Platinum", "Gold", "Silver", "Bronze"
+        ]
+        indiafoss_sponsor_tier: DF.Literal[
+            "Platinum", "Gold", "Silver", "Bronze"
+        ]
+        is_current_partner: DF.Check
+        is_fosshack_sponsor: DF.Check
+        is_indiafoss_sponsor: DF.Check
+        joining_date: DF.Date | None
+        logo: DF.AttachImage
+        special_category: DF.Data | None
+        tier: DF.Literal[
+            "Patron",
+            "Platinum",
+            "Gold",
+            "Silver",
+            "Bronze",
+            "Special",
+            "Flagship Event Sponsor",
+        ]
+        website: DF.Data
+    # end: auto-generated types
+
     pass
