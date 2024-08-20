e71e01fb0f304d8a4de81f4c5afa0e0863247e48
harshtandiya <harsh.tandiya@gmail.com>
2024-08-20 17:49:24 +0530

    test(chapter): test role assignment on create

--- fossunited/chapters/doctype/foss_chapter/test_foss_chapter.py
+++ fossunited/chapters/doctype/foss_chapter/test_foss_chapter.py
@@ -1,8 +1,49 @@
 # Copyright (c) 2023, Frappe x FOSSUnited and Contributors
 # See license.txt
 
+import frappe
+from faker import Faker
 from frappe.tests.utils import FrappeTestCase
 
 
 class TestFOSSChapter(FrappeTestCase):
-    pass
+    def setUp(self):
+        fake = Faker()
+
+        chapter = frappe.get_doc(
+            {
+                "doctype": "FOSS Chapter",
+                "chapter_name": "Pune",
+                "chapter_type": "City Community",
+                "city": "Pune",
+                "country": "India",
+                "email": fake.email(),
+                "facebook": fake.url(),
+                "instagram": fake.url(),
+                "linkedin": fake.url(),
+                "mastodon": fake.url(),
+                "public_chat_group_url": fake.url(),
+                "state": "Maharashtra",
+                "x": fake.url(),
+            }
+        )
+        chapter.insert()
+
+        lead_profile = frappe.get_doc("FOSS User Profile", {"user": "test@example.com"})
+        chapter.append("chapter_members", {"chapter_member": lead_profile.name, "role": "Lead"})
+        chapter.save()
+
+        self.chapter = chapter
+
+    def tearDown(self):
+        frappe.delete_doc("FOSS Chapter", self.chapter.name)
+
+    def test_role_assignment_on_create(self):
+        # Given a chapter
+        chapter = self.chapter
+
+        # When the chapter was created, a lead was assigned to it.
+        # Then the lead should have the role of 'Chapter Lead' and 'Chapter Team Member'
+        user = frappe.db.get_value("FOSS User Profile", chapter.chapter_members[0].chapter_member, "user")
+        has_role = frappe.db.exists("Has Role", {"role": "Chapter Team Member", "parent": user})
+        self.assertTrue(has_role)
