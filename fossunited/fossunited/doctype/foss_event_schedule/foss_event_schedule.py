dfd1a7428b192d0289a71964876469d78d68c936
harshtandiya <harsh.tandiya@gmail.com>
2024-08-21 16:07:36 +0530

    chore: update FOSS Event Schedule doctype fields
    Update the fields in the FOSS Event Schedule doctype to align with the latest requirements. The changes include renaming the "talk_title" field to "title", adding new fields for "category", "other_category", and "speakers" JSON field, and removing the "speaker" data field.

--- fossunited/fossunited/doctype/foss_event_schedule/foss_event_schedule.py
+++ fossunited/fossunited/doctype/foss_event_schedule/foss_event_schedule.py
@@ -5,4 +5,24 @@ from frappe.model.document import Document
 
 
 class FOSSEventSchedule(Document):
+    # begin: auto-generated types
+    # This code is auto-generated. Do not modify anything in this block.
+
+    from typing import TYPE_CHECKING
+
+    if TYPE_CHECKING:
+        from frappe.types import DF
+
+        category: DF.Literal["Talk", "Lightning Talk", "Workshop", "Panel Discussion", "Opening Note", "Break", "Other"]
+        end_time: DF.Time | None
+        linked_cfp: DF.Link | None
+        other_category: DF.Data | None
+        parent: DF.Data
+        parentfield: DF.Data
+        parenttype: DF.Data
+        scheduled_date: DF.Date | None
+        speakers: DF.JSON | None
+        start_time: DF.Time | None
+        title: DF.Data | None
+    # end: auto-generated types
     pass
