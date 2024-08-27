f5e433681ee241e4343182e89c69d847cd8f0c07
harshtandiya <harsh.tandiya@gmail.com>
2024-08-27 12:03:18 +0530

    feat(schedule): Add ScheduleHeader component for schedule page

---
+++ dashboard/src/components/schedule/ScheduleHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex items-start gap-4">
+    <ScheduleHeroIcon class="min-w-40 hidden md:block" />
+    <div class="flex flex-col gap-2">
+      <div class="prose">
+        <h1 class="font-bond">Schedule</h1>
+      </div>
+      <div class="text-base text-gray-700">
+        {{ event.schedule_page_description }}
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+import ScheduleHeroIcon from '@/components/icons/ScheduleHeroIcon.vue'
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  event: {
+    type: Object,
+    required: true,
+  },
+})
+</script>
