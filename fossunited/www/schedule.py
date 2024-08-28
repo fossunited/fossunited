bbb0c21f318c2f55ef50ceb62aaf2a3cfb5ce40b
harshtandiya <harsh.tandiya@gmail.com>
2024-08-28 11:57:31 +0530

    feat(schedule)!: add redirect from `event/schedule` to `dashboard/schedule`

---
+++ fossunited/www/schedule.py
@@ -0,0 +1,9 @@
+import frappe
+
+
+def get_context(context):
+    event_permalink = frappe.form_dict.get("event_permalink")
+
+    frappe.local.flags.redirect_location = f"/dashboard/schedule/{event_permalink}"
+
+    raise frappe.Redirect
