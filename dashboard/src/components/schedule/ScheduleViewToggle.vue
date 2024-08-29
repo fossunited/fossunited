fb2021fc27ed64a36850e0422d37c3fac5c5309e
harshtandiya <harsh.tandiya@gmail.com>
2024-08-29 14:44:04 +0530

    feat(schedule): Add ScheduleViewToggle component

---
+++ dashboard/src/components/schedule/ScheduleViewToggle.vue
@@ -0,0 +1,43 @@
+<template>
+    <TabGroup @change="changeModelValue">
+        <TabList class="flex gap-6">
+            <Tab v-for="item in tabItems" :key="item.value" v-slot="{ selected }" class="focus-visible:outline-none">
+                <div
+                    class="border border-gray-900 flex items-center justify-center p-2 text-base font-medium hover:bg-gray-100 transition-colors"
+                    :class="{ 'bg-gray-900 text-white hover:bg-gray-800': selected }"
+                >
+                    <component :is="item.icon" :class="{'fill-white': selected}" />
+                </div>
+            </Tab>
+        </TabList>
+    </TabGroup>
+</template>
+<script setup>
+import { defineModel } from 'vue'
+import { TabGroup, TabList, Tab } from '@headlessui/vue'
+import RowIcon from '@/components/icons/RowIcon.vue'
+import ColumnIcon from '@/components/icons/ColumnIcon.vue'
+
+const model = defineModel({
+  prop: 'selectedScheduleView',
+  event: 'change',
+})
+
+const changeModelValue = (index) => {
+  model.value = tabItems[index].value
+}
+
+const tabItems = [
+    {
+        label: 'Vertical',
+        icon: RowIcon,
+        value: 'vertical',
+    },
+    {
+        label: 'Horizontal',
+        icon: ColumnIcon,
+        value: 'horizontal',
+    },
+]
+
+</script>
