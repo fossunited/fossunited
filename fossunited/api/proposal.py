f0958d450d617454c0aecbf8985663ecb0fef3e9
RiyaMathew-11 <riyapmathew2000@gmail.com>
2024-09-01 21:49:39 +0530

    chore: add api to get proposal likes

---
+++ fossunited/api/proposal.py
@@ -0,0 +1,22 @@
+import frappe
+
+from fossunited.fossunited.utils import get_doc_likes
+
+
+@frappe.whitelist()
+def get_likes(proposals) -> dict:
+    """
+    Get proposal like count for a given list of proposals
+
+    Args:
+        proposals (list): The list of proposal ids
+    Returns:
+        dict: with proposal id as key and count of likes as values
+    """
+
+    proposalLikes = dict()
+
+    for proposal in proposals:
+        proposalLikes[proposal] = len(get_doc_likes("FOSS Event CFP Submission", proposal))
+
+    return proposalLikes
