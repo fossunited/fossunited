9ddd62508b527e3432710162d04d88c637c0c303
RiyaMathew-11 <riyapmathew2000@gmail.com>
2024-09-02 16:11:46 +0530

    chore: add components for event and proposal header banners

--- dashboard/src/pages/events/AllProposals.vue
+++ dashboard/src/pages/events/AllProposals.vue
@@ -1,128 +1,113 @@
 <template>
   <Header />
-  <div class="container mx-auto px-4">
+  <div v-if="event.loading" class="mb-8">
+    <p>Loading event details...</p>
+  </div>
+  <div v-else class="container mx-auto px-4">
     <!-- Back button -->
     <Breadcrumb class="mt-2" :items="breadcrumb_items" />
+    <EventHeader
+      :event="event.data"
+      class="my-4 border-b border-gray-900 pb-4"
+    />
 
-    <!-- Banner -->
-    <div class="flex items-center justify-between mb-6">
-      <div class="flex flex-col gap-y-4 lg:w-3/4">
-        <div v-if="event.data" class="flex flex-col gap-y-2">
-          <h3 class="font-semibold text-gray-600">
-            {{ event.data.event_name }}
-          </h3>
-          <p class="text-sm font-medium">
-            {{ event.data.event_location }} |
-            <span
-              v-if="
-                dayjs(event.event_start_date).date() !=
-                dayjs(event.event_end_date).date()
-              "
-            >
-              {{ dayjs(event.event_start_date).format('Do') }} -
-            </span>
-            <span>{{ dayjs(event.event_end_date).format('Do MMM YYYY') }}</span>
-          </p>
+    <div v-if="cfp.data && proposals.data && proposalLikes.data">
+      <AllProposalsBanner :event="event.data" :cfp="cfp.data" />
+
+      <!-- Search and filters -->
+      <div class="flex flex-col gap-y-3 justify-between mb-6 md:flex-row">
+        <div class="md:w-1/2">
+          <input
+            type="text"
+            placeholder="Search"
+            class="w-full px-4 py-2 border rounded-sm bg-gray-300"
+          />
         </div>
+        <div class="flex gap-x-2 items-center">
+          <div
+            class="px-4 py-2 flex gap-x-1 items-center border rounded-sm bg-gray-800 text-white"
+          >
+            <p>Session Type</p>
+            <ChevronDown />
 
-        <!-- Loading state -->
-        <div v-else-if="event.loading" class="mb-8">
-          <p>Loading event details...</p>
-        </div>
+            <!-- <select class="hidden">
+            <option>Session Type</option>
+            <option>Talk</option>
+          </select> -->
+          </div>
 
-        <!-- Talk Proposals section -->
-        <div class="flex flex-col gap-y-2">
-          <h1 class="text-3xl font-semibold">Talk Proposals</h1>
-          <p class="text-gray-700 font-semibold">
-            <span v-if="cfp.data?.proposal_page_description">
-              {{ cfp.data.proposal_page_description }}
-            </span>
-
-            <span v-else-if="event.data?.event_name">
-              Explore the proposals for {{ event.data.event_name }}, that offers
-              a wide array of sessions.
-            </span>
-          </p>
+          <div
+            class="px-4 py-2 flex gap-x-1 items-center border rounded-sm bg-gray-800 text-white"
+          >
+            <p>Status</p>
+            <ChevronDown />
+            <!-- <select>
+            <option>Session Type</option>
+            <option>Talk</option>
+          </select> -->
+          </div>
         </div>
       </div>
-      <AllProposalsBannerImage class="hidden md:block" />
-    </div>
 
-    <!-- Search and filters -->
-    <div class="flex flex-col gap-y-3 justify-between mb-6 md:flex-row">
-      <div class="md:w-1/2">
-        <input
-          type="text"
-          placeholder="Search"
-          class="w-full px-4 py-2 border rounded-sm bg-gray-300"
-        />
-      </div>
-      <div class="space-x-2">
-        <select class="px-4 py-2 border rounded-sm bg-gray-800 text-white">
-          <option>Session Type</option>
-        </select>
-        <select class="px-4 py-2 border rounded-sm bg-gray-800 text-white">
-          <option>Status</option>
-        </select>
-      </div>
-    </div>
-
-    <!-- Talk proposals list -->
-    <div v-if="proposals.data && proposalLikes.data" class="space-y-4">
-      <div
-        v-for="(proposal, index) in proposals.data"
-        :key="index"
-        class="border-b-2 py-4"
-      >
-        <div class="flex justify-between items-start">
-          <div class="flex flex-col gap-y-3.5">
-            <h3 class="text-xl font-semibold">
-              <a
-                @click.prevent="redirectRouteToSameWindow(`${proposal.route}`)"
-                class="hover:text-green-500 transition-colors duration-300 cursor-pointer"
-              >
-                {{ proposal.talk_title }}
-              </a>
-            </h3>
-            <div
-              class="flex flex-col gap-y-3.5 items-start mt-1 md:flex-row md:items-center"
-            >
-              <div class="flex items-center text-xs font-semibold">
-                <span
-                  class="bg-gray-200 px-2 py-1 text-gray-600 rounded-sm mr-2"
-                  >{{ proposal.session_type.toUpperCase() }}</span
+      <!-- Talk proposals list -->
+      <div class="space-y-4">
+        <div
+          v-for="(proposal, index) in proposals.data"
+          :key="index"
+          class="border-b-2 py-4"
+        >
+          <div class="flex justify-between items-start">
+            <div class="flex flex-col gap-y-3.5">
+              <h3 class="text-xl font-semibold">
+                <a
+                  @click.prevent="
+                    redirectRouteToSameWindow(`${proposal.route}`)
+                  "
+                  class="hover:text-green-500 transition-colors duration-300 cursor-pointer"
                 >
-                <div>
+                  {{ proposal.talk_title }}
+                </a>
+              </h3>
+              <div
+                class="flex flex-col gap-y-3.5 items-start mt-1 md:flex-row md:items-center"
+              >
+                <div class="flex items-center text-xs font-semibold">
                   <span
-                    class="px-2 py-1 rounded-sm md:hidden"
-                    :class="getStatusClass(proposal.status)"
-                    >{{ proposal.status.toUpperCase() }}</span
+                    class="bg-gray-200 px-2 py-1 text-gray-600 rounded-sm mr-2"
+                    >{{ proposal.session_type.toUpperCase() }}</span
                   >
+                  <div>
+                    <span
+                      class="px-2 py-1 rounded-sm md:hidden"
+                      :class="getStatusClass(proposal.status)"
+                      >{{ proposal.status.toUpperCase() }}</span
+                    >
+                  </div>
+                </div>
+                <div v-if="proposal.status == `Approved`" class="flex text-sm">
+                  <SpeakerIcon />
+                  <span>
+                    {{ proposal.full_name }}
+                  </span>
                 </div>
               </div>
-              <div v-if="proposal.status == `Approved`" class="flex text-sm">
-                <SpeakerIcon />
-                <span>
-                  {{ proposal.full_name }}
-                </span>
-              </div>
             </div>
-          </div>
-          <div class="flex flex-col gap-y-3.5 items-end">
-            <div
-              class="flex items-center gap-1 bg-gray-200 px-1.5 py-1 rounded-sm"
-            >
-              <LikesIcon />
-              <span class="text-xs font-semibold">{{
-                proposalLikes.data[proposal.name]
-              }}</span>
-            </div>
-            <div class="mt-2">
-              <span
-                class="px-2 py-1 rounded-sm text-xs font-semibold hidden md:block"
-                :class="getStatusClass(proposal.status)"
-                >{{ proposal.status.toUpperCase() }}</span
+            <div class="flex flex-col gap-y-3.5 items-end">
+              <div
+                class="flex items-center gap-1 bg-gray-200 px-1.5 py-1 rounded-sm"
               >
+                <LikesIcon />
+                <span class="text-xs font-semibold">{{
+                  proposalLikes.data[proposal.name]
+                }}</span>
+              </div>
+              <div class="mt-2">
+                <span
+                  class="px-2 py-1 rounded-sm text-xs font-semibold hidden md:block"
+                  :class="getStatusClass(proposal.status)"
+                  >{{ proposal.status.toUpperCase() }}</span
+                >
+              </div>
             </div>
           </div>
         </div>
@@ -135,18 +120,17 @@
 import { computed } from 'vue'
 import { useRoute } from 'vue-router'
 import { createResource, createListResource, usePageMeta } from 'frappe-ui'
-import dayjs from 'dayjs'
-import advancedFormat from 'dayjs/plugin/advancedFormat'
-import { redirectRouteToSameWindow, redirectRoute } from '@/helpers/utils'
+import { redirectRouteToSameWindow } from '@/helpers/utils'
 import Header from '@/components/Header.vue'
 import Breadcrumb from '@/components/Breadcrumb.vue'
-import AllProposalsBannerImage from '@/components/proposals/AllProposalsBannerImage.vue'
 import SpeakerIcon from '../../components/icons/SpeakerIcon.vue'
 import LikesIcon from '../../components/icons/LikesIcon.vue'
+import AllProposalsBanner from '../../components/proposals/AllProposalsBanner.vue'
+import EventHeader from '../../components/schedule/EventHeader.vue'
+import ChevronDown from '../../components/icons/ChevronDown.vue'
 
 const route = useRoute()
 const eventPermalink = route.params.permalink
-dayjs.extend(advancedFormat)
 
 const event = createResource({
   url: 'frappe.client.get_value',
@@ -162,6 +146,7 @@ const event = createResource({
       'event_bio',
       'is_external_event',
       'external_event_url',
+      'event_logo',
       'route',
     ],
   },
