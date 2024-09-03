6bf3e5ce8720c379485318f700763f497cbec8eb
harshtandiya <harsh.tandiya@gmail.com>
2024-09-03 17:37:14 +0530

    chore(checkin): Add CheckinManageDialog component for attendee check-in management

---
+++ dashboard/src/components/event/CheckinManageDialog.vue
@@ -0,0 +1,141 @@
+<template>
+  <Dialog
+    v-model="showDialog"
+    class="z-50"
+    :options="{
+      title: 'Manage Attendee',
+    }"
+  >
+    <template #body-content v-if="selectedAttendee">
+      <div class="flex flex-col gap-4">
+        <div class="space-y-2">
+          <div class="text-sm uppercase font-medium">Details</div>
+          <div class="bg-gray-50 text-sm p-2 rounded-sm font-mono">
+            <p class="leading-5">
+              Name: {{ selectedAttendee.full_name }}
+              <br />
+              Ticket ID: {{ selectedAttendee.name }}
+              <br />
+              Has Tshirt Add-on:
+              {{ selectedAttendee.wants_tshirt ? 'Yes' : 'No' }}
+              <br />
+            </p>
+            <p v-if="selectedAttendee.wants_tshirt" class="leading-5">
+              <span>Tshirt Size: {{ selectedAttendee.tshirt_size }}</span>
+              <br />
+              <span
+                >Tshirt Assigned:
+                <span
+                  :class="
+                    selectedAttendee.tshirt_assigned
+                      ? 'text-green-600'
+                      : 'text-red-600'
+                  "
+                >
+                  {{ selectedAttendee.tshirt_assigned ? 'Yes' : 'No' }}
+                </span>
+              </span>
+            </p>
+            <div class="border-b-2 border-dashed border-gray-600 my-3"></div>
+            <div class="flex flex-col gap-2">
+              <div class="text-sm uppercase font-medium">Check-ins</div>
+              <div
+                v-for="data in selectedAttendee.checkin_data"
+                class="flex gap-2"
+              >
+                <span>-></span>
+                <span>
+                  {{ dayjs(data.checkin_time).format('DD MMM YYYY, h:mm A') }}
+                </span>
+              </div>
+            </div>
+          </div>
+          <Button
+            v-if="isCheckedInToday(selectedAttendee)"
+            @click="undoAttendeeCheckin.fetch()"
+            class="!text-sm border-orange-600 hover:border-orange-400 text-orange-600"
+            iconLeft="alert-triangle"
+            label="Undo Check-In for Today"
+            size="sm"
+            variant="outline"
+          />
+        </div>
+        <div
+          v-if="
+            selectedAttendee.wants_tshirt && !selectedAttendee.tshirt_assigned
+          "
+        >
+          <hr class="mb-4" />
+          <div class="text-sm uppercase font-medium">Assign T-shirt</div>
+          <p class="text-sm leading-5 mt-1">
+            <span class="text-orange-600">Pending</span> T-shirt assignment.
+            <br />
+            Click the button below when you have assigned a T-shirt to the
+            attendee.
+          </p>
+          <Button
+            class="mt-2"
+            label="Mark as Assigned"
+            size="sm"
+            variant="solid"
+            :loading="assignTshirt.loading"
+            loadingText="Assigning..."
+            @click="assignTshirt.fetch()"
+          />
+        </div>
+      </div>
+    </template>
+  </Dialog>
+</template>
+<script setup>
+import { defineProps, defineModel, inject } from 'vue'
+import { Dialog, createResource } from 'frappe-ui'
+import dayjs from 'dayjs'
+
+const props = defineProps({
+  attendees: Object,
+  selectedAttendee: Object,
+})
+
+const session = inject('$session')
+const route = inject('route')
+const isCheckedInToday = inject('isCheckedInToday')
+
+const showDialog = defineModel()
+
+const assignTshirt = createResource({
+    url: 'fossunited.api.checkins.assign_tshirt',
+    makeParams(){
+        return {
+            event_id: route.params.id,
+            attendee: props.selectedAttendee,
+            user: session.user,
+        }
+    },
+    onSuccess(){
+        const index = props.attendees.data.findIndex(
+            (data) => data.name === props.selectedAttendee.name
+        )
+        props.attendees.data[index].tshirt_assigned = true
+    }
+})
+
+const undoAttendeeCheckin = createResource({
+  url: 'fossunited.api.checkins.undo_attendee_checkin',
+  makeParams() {
+    return {
+      event_id: route.params.id,
+      attendee: props.selectedAttendee,
+      user: session.user,
+    }
+  },
+  onSuccess() {
+    const index = props.attendees.data.findIndex(
+      (data) => data.name === props.selectedAttendee.name,
+    )
+    props.attendees.data[index].checkin_data.shift()
+    props.selectedAttendee = null
+    showDialog.value = false
+  },
+})
+</script>
