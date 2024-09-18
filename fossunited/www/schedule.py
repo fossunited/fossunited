4aca962bf197f4e814fbd41160a6673217fa4709
RiyaMathew-11 <riyapmathew2000@gmail.com>
2024-09-18 15:38:40 +0530

    chore: remove unused files

--- fossunited/www/schedule.py
+++
@@ -1,9 +0,0 @@
-import frappe
-
-
-def get_context(context):
-    event_permalink = frappe.form_dict.get("event_permalink")
-
-    frappe.local.flags.redirect_location = f"/dashboard/schedule/{event_permalink}"
-
-    raise frappe.Redirect
