500b1783b7e6514ffa7ec3efa54052ca43564d43
Harsh Tandiya <harsh.tandiya@gmail.com>
2025-03-12 13:38:53 +0530

    feat(cfp_public): add FormHeader component for proposal submission

---
+++ dashboard/src/components/cfp-public/FormHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex flex-col gap-2 text-black">
+    <h1 class="text-3xl font-bold">Proposal Submission Form</h1>
+    <div class="prose max-w-full" v-html="_description"></div>
+  </div>
+</template>
+
+<script setup>
+import { inject, computed } from 'vue'
+import { cleanedHTML } from '@/helpers/utils'
+
+const cfpData = inject('$cfpData')
+
+const _description = computed(() => {
+  return cleanedHTML(cfpData.data.cfp_form_description)
+})
+</script>
