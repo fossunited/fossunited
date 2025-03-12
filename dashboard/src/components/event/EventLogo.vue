a3829033eb129775c07e495c469d0cced97b9960
Harsh Tandiya <harsh.tandiya@gmail.com>
2025-03-12 13:39:23 +0530

    feat(cfp_public): add EventLogo component to display event logos

---
+++ dashboard/src/components/event/EventLogo.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="w-36 h-36 aspect-square border rounded p-3 flex items-center justify-center">
+    <img :src="logoPath" alt="Event Logo" class="object-contain" />
+  </div>
+</template>
+<script setup>
+defineProps({
+  logoPath: {
+    type: String,
+    required: true,
+  },
+})
+</script>
