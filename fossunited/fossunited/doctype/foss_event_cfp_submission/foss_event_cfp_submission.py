29abaa2f9b93595157f80c3277c62d0a5a5443d8
Harsh Tandiya <harsh.tandiya@gmail.com>
2025-03-13 00:28:00 +0530

    feat(cfp_public): remove required fields from cfp submission schema

--- fossunited/fossunited/doctype/foss_event_cfp_submission/foss_event_cfp_submission.py
+++ fossunited/fossunited/doctype/foss_event_cfp_submission/foss_event_cfp_submission.py
@@ -32,15 +32,15 @@ class FOSSEventCFPSubmission(WebsiteGenerator):
 
         approvability: DF.Data | None
         attendance_confirmed: DF.Check
-        bio: DF.TextEditor
+        bio: DF.TextEditor | None
         chapter: DF.Data | None
         custom_answers: DF.Table[FOSSCustomAnswer]
-        designation: DF.Data
-        email: DF.Data
-        event: DF.Data
+        designation: DF.Data | None
+        email: DF.Data | None
+        event: DF.Data | None
         event_name: DF.Data | None
         first_name: DF.Data | None
-        full_name: DF.Data
+        full_name: DF.Data | None
         intended_audience: DF.Literal["Beginner", "Intermediate", "Advanced"]  # noqa: F821
         is_first_talk: DF.Literal["Yes", "No"]  # noqa: F821
         is_published: DF.Check
