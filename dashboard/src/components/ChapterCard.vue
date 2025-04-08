936bc5fd0bd73747df02734858d5ac5e6715191d
Neeluiiitd <neelu22318@iiitd.ac.in>
2025-04-08 12:48:33 +0000

    included-tab-keyboard-navigation-in-ChapterCard.vue

--- dashboard/src/components/ChapterCard.vue
+++ dashboard/src/components/ChapterCard.vue
@@ -1,29 +1,47 @@
 <template>
-  <Card
-    class="border-2 border-transparent rounded-[8px] hover:border-gray-500 transition-colors hover:cursor-pointer"
-    @click="() => $router.push(`/chapter/${props.chapter.name}`)"
+  <div
+    role="button"
+    tabindex="0"
+    @click="goToChapter"
+    @keyup.enter="goToChapter"
+    @keyup.space.prevent="goToChapter"
+    class="focus:outline-none focus:ring-2 focus:ring-blue-600 rounded-[8px]"
   >
-    <template #actions-left>
-      <FossClubLogo
-        v-if="props.chapter.chapter_type == 'FOSS Club'"
-        class="w-7 h-7"
-      ></FossClubLogo>
-      <CityComunityBranding v-else>{{ props.chapter.chapter_type }}</CityComunityBranding>
-    </template>
-    <div class="flex justify-between items-baseline">
-      <div class="text-lg font-medium">{{ props.chapter.chapter_name }}</div>
-    </div>
-  </Card>
+    <Card
+      class="border-2 border-transparent rounded-[8px] hover:border-gray-500 transition-colors hover:cursor-pointer"
+    >
+      <template #actions-left>
+        <FossClubLogo
+          v-if="props.chapter.chapter_type == 'FOSS Club'"
+          class="w-7 h-7"
+        ></FossClubLogo>
+        <CityComunityBranding v-else>{{ props.chapter.chapter_type }}</CityComunityBranding>
+      </template>
+
+      <div class="flex justify-between items-baseline">
+        <div class="text-lg font-medium">{{ props.chapter.chapter_name }}</div>
+      </div>
+    </Card>
+  </div>
 </template>
 
 <script setup>
+import { useRouter } from 'vue-router'
 import FossClubLogo from '@/components/FossClubLogo.vue'
 import CityComunityBranding from '@/components/CityCommunityBranding.vue'
 
 const props = defineProps({
   chapter: {
     type: Object,
-    default: () => ({}),
+    required: true,
   },
 })
+
+const emit = defineEmits(['selected'])//helps to communicate with parent component
+const router = useRouter()
+
+const goToChapter = () => {
+  emit('selected', props.chapter.chapter_name)
+  router.push(`/chapter/${props.chapter.name}`)
+}
 </script>
