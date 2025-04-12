62faec066a6b98cc379a9c9a8f2db2594a49a6db
Harsh Tandiya <harsh.tandiya@gmail.com>
2025-04-12 23:19:08 +0530

    feat(dashboard): add cfp edit submission pages (#895)
    * refactor: `cfp-public` directorty name to `cfp`
    
    * feat: cfp edit pages

---
+++ dashboard/src/components/cfp-submission-edit/SubmissionHeader.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="p-6 space-y-3">
+    <h4 class="text-base font-medium text-gray-600 uppercase mb-2">Edit Your Proposal</h4>
+    <div class="prose max-w-full">
+      <h2>{{ submission.talk_title }}</h2>
+    </div>
+    <div class="flex gap-2 text-base items-center">
+      <span>{{ submission.event_name }}</span>
+      <Badge :theme="badgeTheme[submission.status]" :label="submission.status" />
+    </div>
+    <Button
+      label="See Public Page"
+      icon-right="arrow-up-right"
+      @click="redirectRoute(submission.route)"
+    />
+  </div>
+</template>
+<script setup>
+import { Badge } from 'frappe-ui'
+import { redirectRoute } from '@/helpers/utils'
+
+defineProps({
+  submission: {
+    type: Object,
+    required: true,
+  },
+})
+
+const badgeTheme = {
+  Pending: 'yellow',
+  Approved: 'green',
+  Rejected: 'red',
+}
+</script>
