52133f89f16d5b6acd5e44f9f23908626ef1a720
Swapnil Panigrahi <pokemonmasterswapnil@gmail.com>
2025-04-16 16:42:59 +0000

    Added app.use(pinia)

--- dashboard/src/main.js
+++ dashboard/src/main.js
@@ -1,6 +1,7 @@
 import './index.css'
 
 import { createApp, provide } from 'vue'
+import { createPinia } from 'pinia'
 import router from './router'
 import App from './App.vue'
 import { session } from './data/session'
@@ -9,10 +10,12 @@ import dayjs from 'dayjs'
 import { Button, Card, Input, setConfig, frappeRequest, resourcesPlugin } from 'frappe-ui'
 
 let app = createApp(App)
+const pinia = createPinia()
 
 setConfig('resourceFetcher', frappeRequest)
 
 app.use(router)
+app.use(pinia)
 app.use(resourcesPlugin)
 
 app.provide('$session', session)
