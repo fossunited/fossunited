c1540d434cbf5e9f4d21136cbcbab41c479e0d26
Harsh Tandiya <harsh.tandiya@gmail.com>
2025-04-20 23:08:05 +0530

    refactor(cfp-public): simplify rendering of session categories and references

--- dashboard/src/components/cfp-public/RenderSessionCategories.vue
+++ dashboard/src/components/cfp-public/RenderSessionCategories.vue
@@ -1,11 +1,14 @@
 <template>
-  <div class="flex flex-wrap gap-2">
-    <div
-      v-for="(category, index) in categories"
-      :key="index"
-      class="p-2 flex gap-2 items-center text-sm rounded border border-gray-500 text-ink-gray-6 bg-gray-50"
-    >
-      {{ category }}
+  <div class="space-y-2">
+    <label class="text-base font-medium text-gray-600">Session Categories</label>
+    <div class="flex flex-wrap gap-2">
+      <div
+        v-for="(category, index) in categories"
+        :key="index"
+        class="p-2 flex gap-2 items-center text-sm rounded border border-gray-500 text-ink-gray-6 bg-gray-50"
+      >
+        {{ category }}
+      </div>
     </div>
   </div>
 </template>
