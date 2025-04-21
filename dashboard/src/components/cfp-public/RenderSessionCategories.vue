c2ef9a4d93ac7bb4afec040549f968313c633fbc
Harsh Tandiya <harsh.tandiya@gmail.com>
2025-04-21 18:03:39 +0530

    chore(cfp-public): display message when no session categories are selected

--- dashboard/src/components/cfp-public/RenderSessionCategories.vue
+++ dashboard/src/components/cfp-public/RenderSessionCategories.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="space-y-2">
     <label class="text-base font-medium text-gray-600">Session Categories</label>
-    <div class="flex flex-wrap gap-2">
+    <div v-if="categories.length > 0" class="flex flex-wrap gap-2">
       <div
         v-for="(category, index) in categories"
         :key="index"
@@ -10,6 +10,7 @@
         {{ category }}
       </div>
     </div>
+    <div v-else class="text-sm text-gray-600">No categories selected</div>
   </div>
 </template>
 <script setup>
