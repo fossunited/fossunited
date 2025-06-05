6892122aeb9dc039837a82991c15f20ab63e0319
Arya Kiran <arya@projectsegfau.lt>
2025-06-05 19:56:45 +0530

    grouping for searches in /events/timeline, ability to download ics for timeline

--- fossunited/api/chapter.py
+++ fossunited/api/chapter.py
@@ -102,7 +102,7 @@ def generate_ics(event_ids: str) -> str:
         e.location = event.event_location
         e.organizer = event.chapter_name + " Community"
         e.description = event.event_description
-        e.url = "https://fossunited.org" + str(event.route)
+        e.url = "https://fossunited.org/" + str(event.route)
         e.begin = event.event_start_date.replace(tzinfo=timezone(timedelta(hours=5, minutes=30)))
         e.end = event.event_end_date.replace(tzinfo=timezone(timedelta(hours=5, minutes=30)))
         c.events.add(e)
