97b408ff9b7dc555f1dbb8d234c3dc92b938feef
Harsh Tandiya <harsh.tandiya@gmail.com>
2025-06-16 19:52:47 +0530

    feat: cfp insights page refactor (#1015)
    * feat(cfp submission):add read perms for reviewer for `status` field
    
    * feat: cfp insights page refactor

---
+++ dashboard/src/components/ui/InsightCard.vue
@@ -0,0 +1,38 @@
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    default: '',
+  },
+  description: {
+    type: String,
+    default: '',
+  },
+  value: {
+    type: String,
+    default: '',
+  },
+  theme: {
+    type: String,
+    default: 'gray',
+  },
+})
+</script>
+<template>
+  <div class="flex flex-col gap-4 justify-between border rounded p-4">
+    <div class="flex flex-col gap-4">
+      <slot name="title">
+        <span class="text-sm font-normal text-gray-600">{{ title }}</span>
+      </slot>
+      <slot name="value">
+        <div class="flex gap-2 items-end">
+          <span class="text-2xl font-semibold">{{ value }}</span>
+          <slot name="post-value"></slot>
+        </div>
+      </slot>
+    </div>
+    <slot name="description">
+      <span v-if="description" class="text-sm text-gray-600">{{ description }}</span>
+    </slot>
+  </div>
+</template>
