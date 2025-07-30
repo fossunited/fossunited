08b88f34afa29f59d4fb47836bc2903673c3221b
Dilip <igoldlip@gmail.com>
2025-07-30 13:53:46 +0530

    init: basic organization doctype skeleton

--- fossunited/fossunited/doctype/organization/organization.py
+++ fossunited/fossunited/doctype/organization/organization.py
@@ -1,8 +1,71 @@
 # Copyright (c) 2023, Frappe x FOSSUnited and contributors
 # For license information, please see license.txt
 
-from frappe.model.document import Document
+from frappe.website.website_generator import WebsiteGenerator
 
 
-class Organization(Document):
+class Organization(WebsiteGenerator):
+    # begin: auto-generated types
+    # This code is auto-generated. Do not modify anything in this block.
+
+    from typing import TYPE_CHECKING
+
+    if TYPE_CHECKING:
+        from frappe.types import DF
+
+        from fossunited.foss_profiles.doctype.organization_team_member.organization_team_member import (  # noqa: E501
+            OrganizationTeamMember,
+        )
+
+        bluesky: DF.Data | None
+        country_of_origin: DF.Data | None
+        discord: DF.Data | None
+        discuss: DF.Data | None
+        instagram: DF.Data | None
+        linkedin: DF.Data | None
+        logo: DF.AttachImage | None
+        mastodon: DF.Data | None
+        matrix: DF.Data | None
+        org_about: DF.TextEditor
+        org_email: DF.Data
+        org_lead: DF.Data | None
+        org_members: DF.Table[OrganizationTeamMember]
+        org_name: DF.Data
+        org_type: DF.Data | None
+        org_website: DF.Data
+        published: DF.Check
+        route: DF.Data | None
+        telegram: DF.Data | None
+        twitter: DF.Data | None
+        # end: auto-generated types
     pass
+
+    def get_social_links(self):
+        socials = {}
+        SOCIAL_LINK_FIELDNAMES = [
+            "github",
+            "gitlab",
+            "x",
+            "linkedin",
+            "instagram",
+            "mastodon",
+            "youtube",
+            "facebook",
+            "matrix",
+            "telegram",
+            "bluesky",
+            "zulip",
+            "whatsapp",
+            "discord",
+        ]
+        for k, v in self.as_dict().items():
+            if k in SOCIAL_LINK_FIELDNAMES:
+                if k == "matrix":
+                    k = "matrix-light"
+
+                if v:
+                    socials[k] = v
+            else:
+                continue
+
+        return socials
