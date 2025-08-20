38403badd49683eb5a1b3353011e3e0e597f0c1b
Dilip <117019901+idlip@users.noreply.github.com>
2025-08-20 22:02:15 +0530

    Update team page (#1099)
    * update teams page to include governance board
    
    * add governance board role to foss united team doctype
    
    * fixup as per review
    
    - change governance -> governing
    - handle extra info safely
    
    * remove limit for team listing
    
    - also removed unused user_bio data
      Maybe will use sometime in future

---
+++ fossunited/fossunited/doctype/foss_united_team/test_foss_united_team.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Frappe x FOSSUnited and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestFOSSUnitedTeam(FrappeTestCase):
+    pass
