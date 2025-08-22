dea10a4fd2f18b696e44234ab949b44504ecc3c1
Dilip <igoldlip@gmail.com>
2025-08-22 16:04:36 +0530

    make header sticky so with scroll it appears as breadcrumb (sticky)
    - helps in navigating long schedule list

--- dashboard/src/components/schedule/SessionListHeader.vue
+++ dashboard/src/components/schedule/SessionListHeader.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="w-full flex justify-between px-7 py-3 bg-gray-900 text-white">
+  <div
+    class="w-full flex justify-between px-7 py-3 bg-gray-900 text-white"
+    :class="{ 'sticky top-15 z-30': view === 'vertical' }"
+  >
     <h3 class="text-lg font-medium">{{ title }}</h3>
     <button
       v-if="collapsible"
@@ -23,6 +26,11 @@ const props = defineProps({
   collapsible: {
     type: Boolean,
     default: false,
+  },
+  view: {
+    type: String,
+    required: true,
+    default: 'vertical',
   },
 })
 
