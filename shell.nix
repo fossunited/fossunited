110bfb184080d818e66547e1422fc131edfeebd6
Dilip <117019901+idlip@users.noreply.github.com>
2025-08-22 11:29:20 +0530

    update dev setup with uv + nix (#1106)
    - updated pre-commit package for ruff
    - adds lsp and other formatter linter to pyproject
    
    Users can use 'uv' tool to setup

---
+++ shell.nix
@@ -0,0 +1,20 @@
+with import <nixpkgs> {};
+let
+  pypkgs = (python311.withPackages ((ps: with ps; [
+	# for nix packaged python package
+	# numpy
+  ])));
+
+in
+  pkgs.mkShell {
+
+	nativeBuildInputs = [ pkgs.bashInteractive ];
+
+	buildInputs = with pkgs; [
+	  # pypkgs
+	  # ruff black
+	  # ty # basedpyright
+	  uv nodePackages.eslint nodePackages.prettier html-tidy nodePackages.yarn
+	];
+
+  }
