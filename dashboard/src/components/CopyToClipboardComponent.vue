94ad6c3b9029e21b809a9fa950de05e9deca2b35
Dilip <igoldlip@gmail.com>
2025-09-29 14:02:29 +0530

    wrap the route component if exceed the width
    - closes #1179

--- dashboard/src/components/CopyToClipboardComponent.vue
+++ dashboard/src/components/CopyToClipboardComponent.vue
@@ -1,10 +1,18 @@
 <template>
   <Tooltip class="w-fit" :text="tooltip_label" :placement="'top'">
-    <Button class="w-fit my-2 font-mono" size="md" icon-left="copy" @click="copyRouteToClipboard">
-      {{ props.route }}
+    <Button
+      class="w-fit my-2 font-mono text-left"
+      size="md"
+      icon-left="copy"
+      @click="copyRouteToClipboard"
+    >
+      <span class="break-all whitespace-normal block">
+        {{ props.route }}
+      </span>
     </Button>
   </Tooltip>
 </template>
+
 <script setup>
 import { Tooltip } from 'frappe-ui'
 import { ref, defineProps } from 'vue'
