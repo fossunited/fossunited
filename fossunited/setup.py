38c42ead073841d59ff279e3496579ae24c9ce58
Dilip <igoldlip@gmail.com>
2025-10-13 13:20:28 +0530

    setup: add email group names to initial setup of frappe

--- fossunited/setup.py
+++ fossunited/setup.py
@@ -163,7 +163,7 @@ def get_custom_fields():
                 "fieldname": "group_type",
                 "fieldtype": "Select",
                 "label": "Group Type",
-                "options": "Chapter Main\nEvent Participants\nCFP Proposers\nAccepted Proposers\nRejected Proposers\nOther",  # noqa: E501
+                "options": "Chapter Event Participants\nChapter CFP Proposers\nEvent Participants\nCFP Proposers\nAccepted Proposers\nRejected Proposers\nOther",  # noqa: E501
                 "default": "Other",
                 "insert_after": "event",
             },
