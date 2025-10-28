0042515ff57b639a777f0f0df6f445e5a9eb3169
Dilip <117019901+idlip@users.noreply.github.com>
2025-10-28 22:51:59 +0530

    Cfp status (#1236)
    * frontend: show cfp status based on reviewer side status
    
    - if reviewer has commented or added review, then its green as "Reviewed"
      if not let status be shown as pending in yellow
    
    * helper: js to get color for reviewed status text
    
    we check if reviewer has added a review to an CFP, then give green color if not yellow
    
    * frontend: update filter to show switch button for reviewer status
    
    - An switch button to filter/show CFP that are not reviewed by user

--- dashboard/src/helpers/reviewer.js
+++ dashboard/src/helpers/reviewer.js
@@ -1,7 +1,9 @@
 export const getStatusBadgeTheme = (status) => {
   switch (status) {
+    case 'Yes':
     case 'Approved':
       return 'green'
+    case 'No':
     case 'Review Pending':
       return 'orange'
     case 'Rejected':
