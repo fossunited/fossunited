c351bee9ca5079f7b2fc3e404425bcff32dc3d94
Dilip <igoldlip@gmail.com>
2025-10-31 10:50:57 +0530

    init: add PWA support for dashboard via vite plugin

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import frappeui from 'frappe-ui/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +12,11 @@ export default defineConfig({
       lucideIcons: true,
     }),
     vue(),
+    VitePWA({
+      workbox: {
+        maximumFileSizeToCacheInBytes: 5 * 1024 ** 2, // 5MB ; default 2MB
+      },
+    }),
   ],
   resolve: {
     alias: {
