b0f9979322aaa550d9ffa75dff475dd15034185c
Dilip <igoldlip@gmail.com>
2025-12-17 12:49:18 +0530

    doctype(cfp-submission): make link field as TEXT field instead of URL
    - helps to simply avoid 140 char limit
    - Most of the gdocs, presentation and drive links are longer
    - even blog links (medium) are long

--- fossunited/fossunited/doctype/cfp_submission_reference/cfp_submission_reference.py
+++ fossunited/fossunited/doctype/cfp_submission_reference/cfp_submission_reference.py
@@ -14,7 +14,7 @@ class CFPSubmissionReference(Document):
     if TYPE_CHECKING:
         from frappe.types import DF
 
-        link: DF.Data
+        link: DF.SmallText
         parent: DF.Data
         parentfield: DF.Data
         parenttype: DF.Data
