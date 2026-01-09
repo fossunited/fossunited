3575eed6963414bf764707233b391296b8187b55
Dilip <igoldlip@gmail.com>
2026-01-09 17:46:56 +0530

    fix to refetch when route switch

--- dashboard/src/pages/mailing/LocalhostMailing.vue
+++ dashboard/src/pages/mailing/LocalhostMailing.vue
@@ -1,49 +1,63 @@
 <template>
   <MailingPage
+    :key="campaignKey"
     doctype="FOSS Hackathon LocalHost"
     :header-component="LocalhostHeader"
-    document-type="FOSS Hackathon LocalHost"
     :get-campaign-params="getCampaignParams"
     info-text="Send mass emails to localhost participants."
   />
 </template>
 
 <script setup>
+import { ref, watch, computed } from 'vue'
+import { useRoute } from 'vue-router'
+import { createDocumentResource, createResource } from 'frappe-ui'
 import MailingPage from '@/components/mailing/MailingPage.vue'
 import LocalhostHeader from '@/components/localhost/LocalhostHeader.vue'
-import { createDocumentResource, createResource, LoadingText } from 'frappe-ui'
-import { useRoute } from 'vue-router'
-import { ref } from 'vue'
 
 const route = useRoute()
+const hackathonChapter = ref(null)
+const campaignKey = ref(0)
 
-const document = createDocumentResource({
+const localhost = createDocumentResource({
   doctype: 'FOSS Hackathon LocalHost',
   name: route.params.id,
   fields: ['parent_hackathon'],
   auto: true,
-  onSuccess(doc) {
-    hackathon.reload({
-      name: doc.parent_hackathon,
-    })
-  },
 })
-
-const hackathonChapter = ref(null)
 
 const hackathon = createResource({
   url: 'fossunited.api.hackathon.get_hackathon',
   auto: false,
-  onSuccess(doc) {
-    hackathonChapter.value = doc.chapter
-  },
 })
 
-const getCampaignParams = (doc) => {
-  return {
-    reference_document: doc.name,
-    chapter: hackathonChapter.value,
-    document_type: 'FOSS Hackathon LocalHost',
-  }
-}
+watch(
+  () => route.params.id,
+  () => {
+    hackathonChapter.value = null
+    localhost.reload()
+    campaignKey.value++
+  },
+)
+
+watch(
+  () => localhost.doc?.parent_hackathon,
+  (parent) => {
+    if (parent) {
+      hackathon.fetch({ name: parent }).then((data) => {
+        hackathonChapter.value = data.chapter
+        campaignKey.value++
+      })
+    } else {
+      hackathonChapter.value = null
+    }
+  },
+  { immediate: true },
+)
+
+const getCampaignParams = (doc) => ({
+  reference_document: doc.name,
+  chapter: hackathonChapter.value,
+  document_type: 'FOSS Hackathon LocalHost',
+})
 </script>
