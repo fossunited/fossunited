a1a8a0415e6cae55610246d59242ac8ac94d45a7
Dilip <117019901+idlip@users.noreply.github.com>
2026-02-11 17:04:33 +0530

    redesign: listing all hackathon projects page (#1417)
    * www(hack-project): show only published
    
    * redesign(hack-projects): add v3 card styling and sorting feature
    
    * show project counts shown
    
    * fix theme setting after dom loads
    
    * fix lints
    
    * add route link to image logo

--- fossunited/www/hackathon/projects.py
+++ fossunited/www/hackathon/projects.py
@@ -4,10 +4,11 @@ from fossunited.doctype_ids import HACKATHON, HACKATHON_PROJECT
 
 
 def get_context(context):
+    context.no_cache = 1
     context.hackathon = frappe.get_doc(HACKATHON, {"permalink": frappe.form_dict.permalink})
     context.projects = frappe.get_all(
         HACKATHON_PROJECT,
-        {"hackathon": context.hackathon.name},
+        {"hackathon": context.hackathon.name, "is_published": 1},
         ["*"],
         page_length=9999,
     )
