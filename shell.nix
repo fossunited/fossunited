df459e8252e39ddfd0699e5ff35a2d921cfa1e30
Dilip <igoldlip@gmail.com>
2026-02-13 12:25:44 +0530

    ci: add frappe semgrep + PR commit check
    - #1422 + #1404

--- shell.nix
+++ shell.nix
@@ -14,7 +14,7 @@ in
 	  # pypkgs
 	  # ruff black
 	  # ty # basedpyright
-	  uv nodePackages.eslint nodePackages.prettier html-tidy nodePackages.yarn
+	  uv nodePackages.eslint nodePackages.prettier html-tidy nodePackages.yarn semgrep
 	  vale harper
 	];
 
