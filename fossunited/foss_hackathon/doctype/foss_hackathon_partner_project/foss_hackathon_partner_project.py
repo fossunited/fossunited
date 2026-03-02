9d05f9c4ea6fe6e4779db1724dbfbbdbcd0d8893
Dilip <igoldlip@gmail.com>
2026-03-02 16:28:46 +0530

    doctype(partner-project): build participanting projects for chosen ones

--- fossunited/foss_hackathon/doctype/foss_hackathon_partner_project/foss_hackathon_partner_project.py
+++ fossunited/foss_hackathon/doctype/foss_hackathon_partner_project/foss_hackathon_partner_project.py
@@ -1,7 +1,15 @@
 # Copyright (c) 2024, Frappe x FOSSUnited and contributors
 # For license information, please see license.txt
 
+import frappe
 from frappe.website.website_generator import WebsiteGenerator
+
+from fossunited.doctype_ids import (
+    HACKATHON,
+    HACKATHON_ISSUE_PR,
+    HACKATHON_PROJECT,
+    HACKATHON_TEAM_MEMBER,
+)
 
 
 class FOSSHackathonPartnerProject(WebsiteGenerator):
@@ -28,4 +36,45 @@ class FOSSHackathonPartnerProject(WebsiteGenerator):
         repo_link: DF.Data | None
         route: DF.Data | None
     # end: auto-generated types
-    pass
+
+    def before_save(self):
+        self.set_route()
+
+    def set_route(self):
+        """route as /fosshack/year/partner-project/project_name"""
+        start_date = frappe.db.get_value(HACKATHON, self.hackathon, "start_date")
+        event_year = str(start_date.year)
+        self.route = f"fosshack/{event_year}/partner-projects/{frappe.scrub(self.project_name)}"
+
+    def get_context(self, context):
+        """Enhance the web page context with additional data"""
+        context.no_cache = 1
+
+        hackathon = frappe.get_doc(HACKATHON, self.hackathon)
+        context.hackathon = hackathon
+
+        # Get all participant projects linked to this partner project
+        participant_projects = frappe.get_all(
+            HACKATHON_PROJECT,
+            filters={
+                "hackathon": self.hackathon,
+                "partner_project": self.name,
+                "is_published": 1,
+            },
+            fields=["name", "title", "short_description", "route", "team"],
+            order_by="creation desc",
+        )
+
+        # Enrich participant projects with team size and likes
+        for project in participant_projects:
+            # Get team size
+            project.team_size = frappe.db.count(
+                HACKATHON_TEAM_MEMBER, filters={"parent": project.team}
+            )
+            project.contributions = frappe.db.count(
+                HACKATHON_ISSUE_PR, filters={"parent": project.name}
+            )
+
+        context.participant_projects = participant_projects
+
+        return context
