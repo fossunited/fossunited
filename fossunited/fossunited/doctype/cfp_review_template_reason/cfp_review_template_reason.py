0a738ba8bd13af3d1255ef72b2d6deafaba349b6
Dilip <igoldlip@gmail.com>
2026-03-07 17:24:21 +0530

    fix(doctype): remove index_web_pages_for_search from all doctypes
    - cloud instances takes too long on "Build Index" during migrate
      atm it takes 3H with 0% progress shown
    
    - with frappe ticket https://support.frappe.io/helpdesk/my-tickets/62172
      they disclosed as many page routes being present
    
    - i assume this might trigger for all doctypes, and we dont use awesome bar extensively in desk
      so safe to remove

--- fossunited/fossunited/doctype/cfp_review_template_reason/cfp_review_template_reason.py
+++ fossunited/fossunited/doctype/cfp_review_template_reason/cfp_review_template_reason.py
@@ -5,4 +5,18 @@ from frappe.model.document import Document
 
 
 class CFPReviewTemplateReason(Document):
+    # begin: auto-generated types
+    # This code is auto-generated. Do not modify anything in this block.
+
+    from typing import TYPE_CHECKING
+
+    if TYPE_CHECKING:
+        from frappe.types import DF
+
+        parent: DF.Data
+        parentfield: DF.Data
+        parenttype: DF.Data
+        reason: DF.Text
+        type: DF.Literal["Accepted", "Rejected", "Not Sure"]
+    # end: auto-generated types
     pass
--- fossunited/fossunited/doctype/foss_custom_answer/foss_custom_answer.py
+++ fossunited/fossunited/doctype/foss_custom_answer/foss_custom_answer.py
@@ -5,4 +5,19 @@ from frappe.model.document import Document
 
 
 class FOSSCustomAnswer(Document):
+    # begin: auto-generated types
+    # This code is auto-generated. Do not modify anything in this block.
+
+    from typing import TYPE_CHECKING
+
+    if TYPE_CHECKING:
+        from frappe.types import DF
+
+        parent: DF.Data
+        parentfield: DF.Data
+        parenttype: DF.Data
+        question: DF.SmallText | None
+        response: DF.LongText | None
+        type: DF.Data | None
+    # end: auto-generated types
     pass
