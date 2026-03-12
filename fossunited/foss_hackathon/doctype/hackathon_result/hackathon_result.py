e86578fa1389abb39c7f8f56e84b703de1b9bfe3
Dilip <igoldlip@gmail.com>
2026-03-12 12:01:32 +0530

    feat(result): add child table for hackathon result

---
+++ fossunited/foss_hackathon/doctype/hackathon_result/hackathon_result.py
@@ -0,0 +1,26 @@
+# Copyright (c) 2026, Frappe x FOSSUnited and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class HackathonResult(Document):
+    # begin: auto-generated types
+    # This code is auto-generated. Do not modify anything in this block.
+
+    from typing import TYPE_CHECKING
+
+    if TYPE_CHECKING:
+        from frappe.types import DF
+
+        cash_prize: DF.Currency
+        forum_post: DF.Data | None
+        parent: DF.Data
+        parentfield: DF.Data
+        parenttype: DF.Data
+        project: DF.Link | None
+        status: DF.Literal["Winner", "Commendation"]
+        team: DF.Link | None
+    # end: auto-generated types
+    pass
