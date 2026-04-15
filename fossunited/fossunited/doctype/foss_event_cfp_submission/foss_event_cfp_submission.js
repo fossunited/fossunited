d992b6f20beed21abf871920aa403cef381960b3
Dilip <igoldlip@gmail.com>
2026-04-15 21:21:58 +0530

    make other review read-only in desk ui

--- fossunited/fossunited/doctype/foss_event_cfp_submission/foss_event_cfp_submission.js
+++ fossunited/fossunited/doctype/foss_event_cfp_submission/foss_event_cfp_submission.js
@@ -17,8 +17,48 @@ const render_talk_categories = (frm) => {
   frm.set_df_property('categories_preview', 'options', html)
 }
 
+function is_reviewer_only() {
+  return (
+    frappe.user.has_role('CFP Reviewer') &&
+    !frappe.user.has_role('System Manager') &&
+    !frappe.user.has_role('Chapter Team Member')
+  )
+}
+
+// Make all fields except the reviews table read-only for CFP Reviewers.
+// Permlevels alone can't restrict level-0 fields for a role that needs
+// write at level 0 to save (required to add review rows).
+function restrict_reviewer_fields(frm) {
+  if (!is_reviewer_only()) return
+  frm.fields.forEach((f) => {
+    if (f.df.fieldname !== 'reviews') {
+      frm.set_df_property(f.df.fieldname, 'read_only', 1)
+    }
+  })
+  frm.refresh_fields()
+
+  const user = frappe.session.user
+  const already_reviewed = (frm.doc.reviews || []).some((r) => r.email === user)
+  const grid = frm.fields_dict.reviews.grid
+  grid.cannot_add_rows = already_reviewed
+  grid.refresh()
+}
+
+// Within a review row dialog: make all fields read-only if it belongs
+// to a different reviewer.
+frappe.ui.form.on('FOSS Event CFP Review', {
+  refresh(frm) {
+    if (!is_reviewer_only()) return
+    if (frm.doc.email && frm.doc.email !== frappe.session.user) {
+      frm.fields.forEach((f) => frm.set_df_property(f.df.fieldname, 'read_only', 1))
+      frm.refresh_fields()
+    }
+  },
+})
+
 frappe.ui.form.on('FOSS Event CFP Submission', {
   refresh(frm) {
     render_talk_categories(frm)
+    restrict_reviewer_fields(frm)
   },
 })
