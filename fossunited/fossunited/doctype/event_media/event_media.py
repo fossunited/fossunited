a1ebaa91ec184b097b4481929583b5f023e35912
Dilip <117019901+idlip@users.noreply.github.com>
2026-07-10 21:13:12 +0530

    feat(indiafoss): archive page to aggregate all indiafoss talks (#1635)
    * doctype(event-media): init media doctype to hold youtube URLs
    
    - Easy to add more event media and show on archive page
    - Can also be expanded to show for other events in aggregation
    
    * doctype(event-media): add options for video type to match proposal fetch
    
    * add some css to help archive page (buttons)
    
    * macro(indiafoss): add control to show button for if_header
    
    * module(event_media): shared funs to operate for archive page
    
    * hardcode common footer for indiafoss
    
    * prepare context for archive page
    
    * doctype(event_media): change label + fetch event also from proposal
    
    - since proposal is like primary key here along with video url
    
    * remove duplicate workshop in type select
    
    * js: add common shared functions for reusability
    
    * feat: indiafoss archive page
    
    * introduce speakers page
    
    * func code to build speaker index with redis cache
    
    - made generic funcs to for other events also we can show up in a page
    
    - for now /indiafoss/speakers will show all speakers and map each talk video for them
    
    * hooks: set redirect route for indiafoss/speaker talk
    
    - plus update to clear speaker cache as it comes
    
    * feat: indiafoss/speakers page to list them all
    
    * map archive cards to first speaker page on preference
    
    * make whole card take to href not just title text
    
    * feat: speaker page for talk details
    
    * remove og image from page

---
+++ fossunited/fossunited/doctype/event_media/event_media.py
@@ -0,0 +1,40 @@
+# Copyright (c) 2026, Frappe x FOSSUnited and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class EventMedia(Document):
+    # begin: auto-generated types
+    # This code is auto-generated. Do not modify anything in this block.
+
+    from typing import TYPE_CHECKING
+
+    if TYPE_CHECKING:
+        from frappe.types import DF
+
+        from fossunited.fossunited.doctype.cfp_submission_speaker.cfp_submission_speaker import (
+            CFPSubmissionSpeaker,
+        )
+
+        duration: DF.Duration | None
+        event: DF.Link | None
+        event_name: DF.Data | None
+        proposal: DF.Link | None
+        proposal_route: DF.Data | None
+        speakers: DF.Table[CFPSubmissionSpeaker]
+        title: DF.Data | None
+        video_type: DF.Literal[
+            "Talk",
+            "Keynote",
+            "Panel Discussion",
+            "Workshop",
+            "Live Stream",
+            "Lightning Talk",
+            "Birds of Feather(BoF)",
+            "Invited Talk",
+        ]
+        video_url: DF.Data | None
+    # end: auto-generated types
+    pass
