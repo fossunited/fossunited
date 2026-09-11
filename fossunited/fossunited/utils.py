8a2b7ed2db7de8a991dd58627beb9f07614e64ac
Dilip <igoldlip@gmail.com>
2026-09-11 10:20:56 +0530

    fix(frappe.request.path) add path to jinja global exec
    * frappe remove it in latest update
    https://github.com/frappe/frappe/pull/42390

--- fossunited/fossunited/utils.py
+++ fossunited/fossunited/utils.py
@@ -45,6 +45,16 @@ def is_user_team_member(chapter, user):
     return False
 
 
+def get_request_path():
+    """Current request path, for use in Jinja templates.
+
+    frappe.request is no longer exposed to the sandboxed Jinja globals
+    (frappe/frappe#42390 removed it from exec_safe_globals).
+    """
+    request = getattr(frappe.local, "request", None)
+    return request.path if request else ""
+
+
 # Filter
 def make_badge(text="Default", size="sm"):
     # stored in the form of (background-color, text-color)
